800ded5def989a6a38a7c39e84beb7791847b553
SNOOPY <haozheliang18@gmail.com>
2026-03-29 19:41:38 +0800

    add refactor generation function and update Jupyter notebook for parameter experimentation

--- study-companion/scripts/generation.py
+++ study-companion/scripts/generation.py
@@ -2,14 +2,14 @@ import ollama
 
 model_name="qwen3.5:4b"
 
-def generate_raw_response(prompt,model=model_name,temperature=1.0,max_tokens=1024):
+def generate_raw_response(prompt,model=model_name,temperature=0.1,num_predict=200):
     response = ollama.generate(
         model=model,
         prompt=prompt,
         raw=True,
         options={
             "temperature": temperature,
-            "num_predict": max_tokens
+            "num_predict": num_predict
         }
     )
     return {
